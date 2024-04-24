f3112ed242a9cdf4b119652e1df4324adcea798f
deveshbeniwal <devbeniwal80@gmail.com>
2024-04-24 11:21:47 +0530

    Card animations and delay while generating cards added

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -7,8 +7,8 @@ public class GameManager : MonoBehaviour
 
     [Header("Grid Settings")]
     [SerializeField] Grid grid;
-    [SerializeField] GameObject grid_prefab;
-    [SerializeField] Transform grid_parent;
+    [SerializeField] Card grid_card_prefab;
+    [SerializeField] Transform grid_card_parent;
 
     [Header("Camera Settings")]
     [SerializeField] CamSizeHandler cam_handler;
@@ -35,20 +35,20 @@ public class GameManager : MonoBehaviour
     }
     private async void Generate_Cards(Vector2Int _grid_size)
     {
+        float centre_x = (grid.GetCellCenterWorld(new Vector3Int(0, 0)).x + grid.GetCellCenterWorld(new Vector3Int(_grid_size.y - 1, 0)).x) * 0.5f;
+        float centre_y = (grid.GetCellCenterWorld(new Vector3Int(0, 0)).y + grid.GetCellCenterWorld(new Vector3Int(0, _grid_size.x - 1)).y) * 0.5f;
+
+        cam_handler.Initialize(new Vector2(centre_x, centre_y));
+
         for (int i = 0; i < _grid_size.y; i++)
         {
             for (int j = 0; j < _grid_size.x; j++)
             {
-                GameObject card = Instantiate(grid_prefab, grid_parent);
+                Card card = Instantiate(grid_card_prefab, grid_card_parent);
                 card.transform.position = grid.GetCellCenterWorld(new Vector3Int(i, j));
 
                 await Task.Delay(100);
             }
         }
-
-        float centre_x = (grid.GetCellCenterWorld(new Vector3Int(0, 0)).x + grid.GetCellCenterWorld(new Vector3Int(_grid_size.y - 1, 0)).x) * 0.5f;
-        float centre_y = (grid.GetCellCenterWorld(new Vector3Int(0, 0)).y + grid.GetCellCenterWorld(new Vector3Int(0, _grid_size.x - 1)).y) * 0.5f;
-
-        cam_handler.Initialize(new Vector2(centre_x, centre_y));
     }
 }
