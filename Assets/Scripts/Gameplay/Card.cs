55a0f2dde75ad1055e57c5a942e18997b3fd35dc
deveshbeniwal <devbeniwal80@gmail.com>
2024-04-24 12:21:42 +0530

    added card pictures and added splashing card for player to memorize

--- Assets/Scripts/Gameplay/Card.cs
+++ Assets/Scripts/Gameplay/Card.cs
@@ -2,4 +2,80 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    [SerializeField] Animator anim;
+    [SerializeField] BoxCollider2D box_collider;
+
+    [Space]
+    [SerializeField] SpriteRenderer sr_card;
+    [SerializeField] Sprite spr_card_back;
+
+    Sprite card_picture;
+    int card_picture_index;
+
+    bool isShowingCard;
+    bool isAnimProcessing;
+
+    public void Initialize(Vector3 _position, Sprite _card_picture, int _card_picture_index)
+    {
+        transform.position = _position;
+        gameObject.name = _card_picture_index.ToString();
+
+        anim.Play("Init");
+
+        card_picture = _card_picture;
+        card_picture_index = _card_picture_index;
+
+        GameManager.instance.Event_ShowCards += Instance_Event_ShowCards;
+        GameManager.instance.Event_HideCards += Instance_Event_HideCards;
+        GameManager.instance.Event_StartGame += Instance_Event_StartGame;
+    }
+    private void OnDestroy()
+    {
+        GameManager.instance.Event_ShowCards -= Instance_Event_ShowCards;
+        GameManager.instance.Event_HideCards -= Instance_Event_HideCards;
+        GameManager.instance.Event_StartGame -= Instance_Event_StartGame;
+    }
+
+
+
+    private void Instance_Event_ShowCards()
+    {
+        isShowingCard = true;
+        anim.Play("Flip");
+    }
+    private void Instance_Event_HideCards()
+    {
+        isShowingCard = false;
+        anim.Play("Flip");
+    }
+    private void Instance_Event_StartGame()
+    {
+        box_collider.enabled = true;
+    }
+
+    private void Anim_Event_Flipcard()
+    {
+        sr_card.sprite = isShowingCard ? card_picture : spr_card_back;
+    }
+    private void Anim_Event_Anim_Started()
+    {
+        isAnimProcessing = true;
+    }
+    private void Anim_Event_Anim_Stopped()
+    {
+        isAnimProcessing = false;
+    }
+
+
+
+    private void OnMouseDown()
+    {
+        if (isAnimProcessing)
+            return;
+
+        if (isShowingCard)
+            return;
+
+        Debug.Log(gameObject.name);
+    }
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Threading.Tasks;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    public event Action Event_ShowCards;
+    public event Action Event_HideCards;
+    public event Action Event_StartGame;
+
 
     [Header("Grid Settings")]
     [SerializeField] Grid grid;
@@ -13,6 +19,9 @@ public class GameManager : MonoBehaviour
     [Header("Camera Settings")]
     [SerializeField] CamSizeHandler cam_handler;
 
+    [Header("Cards Settings")]
+    [SerializeField] Sprite[] all_card_pictures;
+
     public void Awake()
     {
         if (instance == null)
@@ -21,7 +30,7 @@ public class GameManager : MonoBehaviour
             Destroy(this.gameObject);
     }
 
-    public void Initialize()
+    public async void Initialize()
     {
         Vector2Int grid_size = DataManager.Instance.GameMode switch
         {
@@ -31,24 +40,43 @@ public class GameManager : MonoBehaviour
             _ => new Vector2Int(2, 3)
         };
 
-        Generate_Cards(grid_size);
+        await Generate_Cards(grid_size);
+        await Splash_Cards();
     }
-    private async void Generate_Cards(Vector2Int _grid_size)
+    private async Task Generate_Cards(Vector2Int _grid_size)
     {
         float centre_x = (grid.GetCellCenterWorld(new Vector3Int(0, 0)).x + grid.GetCellCenterWorld(new Vector3Int(_grid_size.y - 1, 0)).x) * 0.5f;
         float centre_y = (grid.GetCellCenterWorld(new Vector3Int(0, 0)).y + grid.GetCellCenterWorld(new Vector3Int(0, _grid_size.x - 1)).y) * 0.5f;
 
         cam_handler.Initialize(new Vector2(centre_x, centre_y));
 
+        int _selected_card_index = UnityEngine.Random.Range(0, all_card_pictures.Length);
+        int _same_card_counts = 0;
+
         for (int i = 0; i < _grid_size.y; i++)
         {
             for (int j = 0; j < _grid_size.x; j++)
             {
-                Card card = Instantiate(grid_card_prefab, grid_card_parent);
-                card.transform.position = grid.GetCellCenterWorld(new Vector3Int(i, j));
+                if(_same_card_counts >= 2)
+                {
+                    _selected_card_index = UnityEngine.Random.Range(0, all_card_pictures.Length);
+                    _same_card_counts = 0;
+                }
 
+                Card card = Instantiate(grid_card_prefab, grid_card_parent);
+                card.Initialize(grid.GetCellCenterWorld(new Vector3Int(i, j)), all_card_pictures[_selected_card_index], _selected_card_index);
+
+                _same_card_counts++;
                 await Task.Delay(100);
             }
         }
+    }
+    private async Task Splash_Cards()
+    {
+        Event_ShowCards?.Invoke();
+        await Task.Delay(2000);
+        Event_HideCards?.Invoke();
+        await Task.Delay(1000);
+        Event_StartGame?.Invoke();
     }
 }
